c40560e623ff3f79c6c92625624d02a4c1bd3d8c
Abdul <abdlhanan987@gmail.com>
2025-04-13 19:08:10 +0500

    Removing unnecessary files

--- obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49af81b6c41137c6f41b93cd92f89261aaf76366")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996096652582e6cf475f892a9e328628cd623ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
